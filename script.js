70bf61d48dbb19d1406caaefcc834de746568327
Xuan Do <doxuancntp@gmail.com>
2019-03-25 17:06:13 -0400

    Adding A Simple Callback Function.

--- script.js
+++ script.js
@@ -10,11 +10,88 @@
 
 let img = document.getElementById('img');
 let h2 = document.getElementsByTagName('h2');
+console.log(h2);
 
 // TweenLite.to(img, 1, { width: 100 });
 // TweenLite.to(img, 1, { x: 200 });
 // TweenLite.from(img, 1, { x: -200 });
 // TweenLite.fromTo(img, 1, { x: -200 }, { x: 200 });
 // TweenLite.set(img, { x: -200 });
-TweenLite.from(img, 1, { x: -200, ease: SteppedEase.config(20) });
-TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1 });
+// TweenLite.from(img, 1, { x: -200, ease: SteppedEase.config(20) });
+// TweenLite.from(h2, 1, { autoAlpha: 0, delay: 1 });
+
+let i = 0;
+
+TweenLite.from(img, 1, {
+    x: -200,
+    ease: Power1.easeInOut,
+    onStart: onStart,
+    onUpdate: onUpdate,
+    onComplete: onComplete
+});
+
+function onStart() {
+    console.log("onStart");
+}
+
+function onUpdate() {
+    console.log("onUpdate");
+
+    h2[0].innerHTML = "" + i++;
+}
+
+function onComplete() {
+    console.log("onComplete");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
