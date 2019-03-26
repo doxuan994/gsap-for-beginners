1ebaec48299fa6e6f1ad6eee0ebb9c7f2be40e4e
Xuan Do <doxuancntp@gmail.com>
2019-03-26 10:54:46 -0400

    Commit.

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ console.log(loader);
 
 
 // tlLoader = new TimelineLite();
-tlLoader = new TimelineMax({repeat: -1});
+tlLoader = new TimelineMax({repeat: 2, onComplete: loadContent});
 
 
 
@@ -138,7 +138,23 @@ tlLoader
 
 
 
+function loadContent() {
+    let tlLoaderOut = new TimelineLite({ onComplete: contentIn });
+    tlLoaderOut
+        .set(dot, { backgroundColor: 'black' })
+        .to(loader, 0.3, { autoAlpha: 1, scale: 1.3, ease: Power0.easeNone })
+        .staggerFromTo(dot, 0.3,
+            { y: 0, autoAlpha: 0 },
+            { y: 20, autoAlpha: 1, ease: Back.easeInOut },
+            0.05, 0
+        )
+        .to(loader, 0.3, { y: -150, autoAlpha: 0, ease: Back.easeIn }, '+=0.3');
+}
 
+
+function contentIn() {
+    tl.play();
+}
 
 
 
