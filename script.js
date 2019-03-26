2c946150cb35bbd3d54aebb1f090c115c4a4fd65
Xuan Do <doxuancntp@gmail.com>
2019-03-25 21:29:01 -0400

    Adding Tweens To A Timeline.

--- script.js
+++ script.js
@@ -73,18 +73,31 @@ let tl = new TimelineLite();
 
 
 
+// tl
+// .from(h1, 0.3, { y: -15, autoAlpha: 0, ease: Power1.easeOut })
+// .from(intro, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
+// .from(img, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
+// .from(h2, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
+// .from(listItem, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15');
+
+
+
+// tl
+// .from(h1, 0.3, { y: -15, autoAlpha: 0, ease: Power1.easeOut })
+// .from(intro, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '+=1')
+// .from(img, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 3)
+// .from(h2, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 2)
+// .from(listItem, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 2.5);
+
+
+
 tl
 .from(h1, 0.3, { y: -15, autoAlpha: 0, ease: Power1.easeOut })
-.from(intro, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
-.from(img, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
-.from(h2, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15')
-.from(listItem, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, '-=0.15');
-
-
-
-
-
-
+.add('intro')
+.from(intro, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut })
+.from(img, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 'intro')
+.from(h2, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 'intro+=3')
+.from(listItem, 0.3, { y:-15, autoAlpha: 0, ease: Power1.easeOut }, 'intro');
 
 
 
